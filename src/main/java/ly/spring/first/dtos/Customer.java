7fb8466b02655cae984f59e017e1ff35c7a78f27
Shiva Pratap Shah <shah.shiva@gmail.com>
2019-04-19 13:19:45 -0400

    commit 1 Uploading samplespring

--- src/main/java/ly/spring/first/dtos/Customer.java
+++ src/main/java/ly/spring/first/dtos/Customer.java
@@ -1,33 +1,61 @@
 package ly.spring.first.dtos;
 
 public class Customer {
-private int id;
-private String fullName;
-private String email;
-private String phone;
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getFullName() {
-	return fullName;
-}
-public void setFullName(String fullName) {
-	this.fullName = fullName;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public String getPhone() {
-	return phone;
-}
-public void setPhone(String phone) {
-	this.phone = phone;
-}
+	private int id;
+	private String fullName;
+	private String email;
+	private String phone;
+	
+	private Address address;
+	
 
-}
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", fullName=" + fullName + ", email=" + email + ", phone=" + phone + ", address="
+				+ address + "]";
+	}
+
+	
+	}
+
+
--- src/main/java/ly/spring/first/main/MainApplication.java
+++ src/main/java/ly/spring/first/main/MainApplication.java
@@ -5,13 +5,15 @@ import ly.spring.first.dtos.Customer;
 public class MainApplication {
 
 	public static void main(String[] args) {
-	
-		Customer c=new Customer();
+
+		Customer c = new Customer();
 		c.setId(1);
 		c.setFullName("Shiva Pratap Shah");
 		c.setEmail("shah.shiva@gmail.com");
 		c.setPhone("17577061966");
+		
+	}
 
 	}
 
-}
+
